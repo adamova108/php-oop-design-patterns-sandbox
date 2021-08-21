2099ab90ea0eeb255fbd7a7b008d9ac62304f559
Adam L <luzsiadam@gmail.com>
2021-08-21 08:54:39 +0200

    + Added dummy PHPUnit test

---
+++ BookTitleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use KWD_Sandbox\BookTitle;
+use PHPUnit\Framework\TestCase;
+
+class BookTitleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testBookTitle()
+    {
+        $bookTitle = new BookTitle("The Full Potential Within");
+        $this->assertEquals("The Full Potential Within", $bookTitle->getTitleString());
+    }
+
+    /**
+     * @test
+     */
+    public function testEmptyBookTitle()
+    {
+        $this->expectException(\Exception::class);
+        new BookTitle("");
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitdebf191cd190295660a5d369256de290
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitdebf191cd190295660a5d369256de290', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitdebf191cd190295660a5d369256de290', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInitdebf191cd190295660a5d369256de290
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitdebf191cd190295660a5d369256de290::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiredebf191cd190295660a5d369256de290($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequiredebf191cd190295660a5d369256de290($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }

e67d47351a4848f61bec8d96ff4d11b6ecc9c33c
Adam L <luzsiadam@gmail.com>
2021-09-25 19:43:04 +0200

    + Updated Foo testclass

---
+++ src/Foo.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Foo class, does foo
+ *
+ * @package      AL_Inpsyde
+ * @license      GPL-2.0+
+ */
+
+declare(strict_types=1);
+
+namespace KWD_Sandbox;
+
+
+/**
+ * Foo class.
+ */
+class Foo
+{
+    /**
+     * Bar.
+     *
+     * @return string
+     */
+    public function bar(): string
+    {
+        return 'Foo::bar()';
+    }
+
+    /**
+     * Returns true, always.
+     *
+     * @return true
+     */
+    public function is_true(): bool
+    {
+        return true;
+    }
+
+    public function is_false(): bool
+    {
+        return false;
+    }
+}
